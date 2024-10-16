936605df9abc208ab352b5617be96fe0638c018e
Sergey Boykov <sutemi67@gmail.com>
2024-10-16 22:56:25 +0300

    Initial commit

---
+++ app/src/main/java/apc/appcradle/trainingdiary/MainActivity.kt
@@ -0,0 +1,28 @@
+package apc.appcradle.trainingdiary
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import apc.appcradle.trainingdiary.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navHostController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostController.navController
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/apc/appcradle/trainingdiary/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package apc.appcradle.trainingdiary.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
