5acd2c3c5eba125264459b700fdaf5c9f77ac2cd
Sergey Boykov <sutemi67@gmail.com>
2024-10-23 10:45:58 +0300

    timer patch

--- app/src/main/java/apc/appcradle/trainingdiary/ui/timer/TimerFragment.kt
+++ app/src/main/java/apc/appcradle/trainingdiary/ui/timer/TimerFragment.kt
@@ -2,20 +2,14 @@ package apc.appcradle.trainingdiary.ui.timer
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import apc.appcradle.trainingdiary.databinding.FragmentTimerBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TimerFragment : Fragment() {
-    private lateinit var timerTextView: TextView
-    private lateinit var countDownTimer: CountDownTimer
-    private var isRunning = false
-    private var timeLeftInMillis: Long = 10000 // 1 минута
     private lateinit var binding: FragmentTimerBinding
     private val vm by viewModel<TimerViewModel>()
 
@@ -29,45 +23,30 @@ class TimerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        timerTextView = binding.counter
-        binding.startButton.setOnClickListener {
-            if (!isRunning) {
-                startTimer()
-            } else {
-                countDownTimer.cancel()
-                startTimer()
-            }
+        setOnClickListeners()
+        vm.timeLeft.observe(viewLifecycleOwner) { time ->
+            updateTimerText(time)
         }
-    }
-
-    private fun startTimer() {
-        if (binding.input.text.isNullOrEmpty()) {
-            return
-        }
-        timeLeftInMillis = binding.input.text.toString().toLong() * 1000
-        countDownTimer = object : CountDownTimer(timeLeftInMillis, 1000) {
-
-            override fun onTick(millisUntilFinished: Long) {
-                timeLeftInMillis = millisUntilFinished
-                updateTimerText()
-            }
-
-            override fun onFinish() {
-                vm.sendNotification("Timer has ended")
-                isRunning = false
-            }
-        }
-        countDownTimer.start()
-        isRunning = true
     }
 
     @SuppressLint("DefaultLocale")
-    private fun updateTimerText() {
-        val minutes = (timeLeftInMillis / 1000) / 60
-        val seconds = (timeLeftInMillis / 1000) % 60
+    private fun updateTimerText(time: Long) {
+        val hours = (time / 1000) / 360
+        val minutes = (time / 1000) / 60
+        val seconds = (time / 1000) % 60
+        val timeFormatted = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+        binding.counter.text = timeFormatted
+    }
 
-        val timeFormatted = String.format("%02d:%02d", minutes, seconds)
-        timerTextView.text = timeFormatted
+    private fun setOnClickListeners() {
+        binding.startButton.setOnClickListener {
+            if (!binding.input.text.isNullOrEmpty()) {
+                vm.startTimer(binding.input.text.toString().toLong())
+            }
+        }
+        binding.stopButton.setOnClickListener {
+            vm.stopTimer()
+        }
     }
 
     companion object {
--- app/src/main/java/apc/appcradle/trainingdiary/ui/timer/TimerViewModel.kt
+++ app/src/main/java/apc/appcradle/trainingdiary/ui/timer/TimerViewModel.kt
@@ -1,11 +1,43 @@
 package apc.appcradle.trainingdiary.ui.timer
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import apc.appcradle.trainingdiary.domain.NotificationServiceInterface
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class TimerViewModel(
     private val notification: NotificationServiceInterface
 ) : ViewModel() {
 
-    fun sendNotification(text: String) = notification.sendNotification(text)
+    private var timerJob: Job? = null
+    private var timeLeftInMillis: Long = 0
+
+    private val _timeLeft = MutableLiveData(0L)
+    val timeLeft: LiveData<Long> = _timeLeft
+
+    private fun sendNotification(text: String) = notification.sendNotification(text)
+
+    fun startTimer(timeLeft: Long) {
+        if (timerJob == null || timerJob?.isCancelled == true) {
+            timeLeftInMillis = timeLeft * 1000
+            timerJob = viewModelScope.launch {
+                while (timeLeftInMillis != 0L) {
+                    delay(1000L)
+                    timeLeftInMillis -= 1000L
+                    _timeLeft.postValue(timeLeftInMillis)
+                }
+                stopTimer()
+                sendNotification("Timer ended")
+            }
+        }
+    }
+
+    fun stopTimer() {
+        timerJob?.cancel()
+    }
+
 }
